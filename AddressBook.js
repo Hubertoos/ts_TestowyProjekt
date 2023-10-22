69b17a27c835a2f4bb586530d148858f89a436e1
Hubertoos <Hubertoos@users.noreply.github.com>
2023-10-22 13:21:34 +0200

    dodanie klas

--- AddressBook.js
+++ AddressBook.js
@@ -6,27 +6,112 @@ var Contact = /** @class */ (function () {
     function Contact(name, lastName) {
         this.name = name;
         this.lastName = lastName;
-        this.setThisDate();
+        this.createDate = new Date();
+        this.setModDate(new Date());
+        this.setUUID();
+        this.email = '';
     }
+    Contact.prototype.setName = function (newName) {
+        this.name = newName;
+    };
     Contact.prototype.getName = function () {
         return this.name;
+    };
+    Contact.prototype.setLastName = function (newLastName) {
+        this.lastName = newLastName;
     };
     Contact.prototype.getLastName = function () {
         return this.lastName;
     };
-    Contact.prototype.setName = function (newName) {
-        this.name = newName;
+    Contact.prototype.setModDate = function (newModDate) {
+        this.modDate = newModDate;
     };
-    Contact.prototype.setThisDate = function () {
-        this.date = new Date();
+    Contact.prototype.getModDAte = function () {
+        return this.modDate;
+    };
+    Contact.prototype.setUUID = function () {
+        var calcUUID = '1234-5678-9012';
+        this.uuid = calcUUID;
+    };
+    Contact.prototype.setExternalUUID = function (newExtUIID) {
+        this.uuid = newExtUIID;
+    };
+    Contact.prototype.getUUID = function () {
+        return this.uuid;
+    };
+    Contact.prototype.setEmail = function (newEmail) {
+        this.email = newEmail;
+    };
+    Contact.prototype.getEmail = function () {
+        return this.email;
     };
     return Contact;
 }());
+var Group = /** @class */ (function () {
+    function Group(groupName, groupUUID) {
+        this.groupName = groupName;
+        this.groupUUID = groupUUID;
+        this.contactList = [];
+    }
+    Group.prototype.addContactToGroup = function (newContact) {
+        this.contactList.push(newContact);
+    };
+    Group.prototype.removeContactFromGroup = function (rmvContact) {
+        var test = this.contactList.map(function (cList) {
+            console.log(cList.getUUID() + '|' + rmvContact.getUUID());
+            //cList.getUUID() != rmvContact.getUUID();
+            cList.setEmail(rmvContact.getName());
+        });
+        console.log(test);
+    };
+    Group.prototype.findContactInGroupByUIID = function (name, lastName, findUUID) {
+        //return this.contactList.forEach( c => console.log(c.getUUID()));
+        return this.contactList.some(function (cntct) { return cntct.getUUID() == findUUID; });
+    };
+    return Group;
+}());
+var AddressBook = /** @class */ (function () {
+    function AddressBook() {
+        this.allContacts = [];
+        this.allGroups = [];
+    }
+    AddressBook.prototype.addContact = function (newContact) {
+        this.allContacts.push(newContact);
+    };
+    AddressBook.prototype.addGroup = function (newGroup) {
+        this.allGroups.push(newGroup);
+    };
+    return AddressBook;
+}());
 //const Person = new Contact("Jan", "Kowalski", new Date("2023-09-15"));
 var Person = new Contact("Jan", "Kowalski");
-console.log(Person);
-console.log(Person.getLastName());
-console.log(Person.getName());
+// console.log(Person);
+// console.log(Person.getLastName());
+// console.log(Person.getName());
 Person.setName("Mietek");
-console.log(Person.getName());
+// console.log(Person.getName());
+Person.setEmail('Jan.Kowalski@gmail.com');
+// console.log(Person);
+Person.setExternalUUID('9876-5432-1098');
+// console.log(Person.getUUID());
+var Grupa = new Group('Nowa grupa', 'Nowy UUID');
+var Grupa1 = new Group('Grupa1', 'UUID_1111');
+// console.log(Grupa);
+Grupa.addContactToGroup(Person);
+// console.log(Grupa);
+var Person1 = new Contact("Janina", "Kowalska");
+Grupa.addContactToGroup(Person1);
+console.log(Grupa);
+Grupa1.addContactToGroup(Person);
+var AdresBuk = new AddressBook();
+AdresBuk.addContact(Person);
+AdresBuk.addGroup(Grupa);
+AdresBuk.addGroup(Grupa1);
+// console.log(AdresBuk);
+// console.log(AdresBuk.allGroups.forEach(e => e.contactList.forEach(c => console.log(c.getName()))));
+// console.log(Grupa.findContactInGroupByUIID('1234-5678-9012'));
+console.log("Remove:");
+Grupa.addContactToGroup(Person);
+Grupa.removeContactFromGroup(Person1);
+console.log(Grupa);
 //# sourceMappingURL=AddressBook.js.map
--- AddressBook.ts
+++ AddressBook.ts
@@ -3,7 +3,10 @@ class Contact {
     // Ma umożliwiać: aktualizację datę modyfikacji, pozwalac na modyfikację imienia, nazwiska oraz adresu email
     private name: string;
     private lastName: string;
-    private date: Date;
+    private email: string;
+    private createDate: Date;
+    private modDate: Date;
+    private uuid: string;
 
     // public constructor(name: string, lastName: string) {
     //     this.name = name;
@@ -13,32 +16,143 @@ class Contact {
     public constructor(name: string, lastName: string) {
         this.name = name;
         this.lastName = lastName;
-        this.setThisDate();
+        this.createDate = new Date();
+        this.setModDate(new Date());
+        this.setUUID();
+        this.email = '';
     }
+ 
+    public setName(newName: string) {
+            this.name=newName;
+        }
 
     public getName(): string {
         return this.name;
+    }
+
+    public setLastName(newLastName: string) {
+        this.lastName = newLastName;
     }
 
     public getLastName(): string {
         return this.lastName;
     }
 
-    public setName(newName: string) {
-        this.name=newName;
+    public setModDate(newModDate: Date) {
+        this.modDate = newModDate;
     }
 
-    private setThisDate() {
-        this.date = new Date();
+    public getModDAte(): Date{
+        return this.modDate;
+    }
+
+    public setUUID() {
+        let calcUUID = '1234-5678-9012';
+        this.uuid = calcUUID;
+    }
+
+    public setExternalUUID(newExtUIID: string) {
+        this.uuid = newExtUIID;
+    }
+
+    public getUUID(): string {
+        return this.uuid;
+    }
+
+    public setEmail(newEmail: string) {
+        this.email = newEmail;
+    }
+
+    public getEmail(): string {
+        return this.email;
     }
 }
 
+
+
+class Group {
+
+    contactList: Contact[];
+    groupName: string;
+    groupUUID:string;
+
+    public constructor (groupName: string, groupUUID: string) {
+        this.groupName = groupName;
+        this.groupUUID = groupUUID;
+        this.contactList = [];
+    }
+
+    public addContactToGroup(newContact: Contact) {
+        this.contactList.push(newContact);
+    }
+
+    public removeContactFromGroup(rmvContact: Contact) {
+        this.contactList.filter(cList => {
+            console.log(cList.getUUID()+'|'+ rmvContact.getUUID());
+            //cList.getUUID() != rmvContact.getUUID();
+            cList!==rmvContact;
+        });
+    }
+
+
+
+    public findContactInGroupByUIID (name? : string, lastName? : string, findUUID?: string) {
+        //return this.contactList.forEach( c => console.log(c.getUUID()));
+        return this.contactList.some(cntct => cntct.getUUID()==findUUID);
+    }
+}
+
+class AddressBook {
+    allContacts: Contact[];
+    allGroups: Group[];
+
+    public constructor () {
+        this.allContacts = [];
+        this.allGroups = [];
+    }
+
+    public addContact(newContact: Contact) {
+        this.allContacts.push(newContact);
+    }
+
+    public addGroup(newGroup: Group){
+        this.allGroups.push(newGroup);
+    }
+}
+
+
 //const Person = new Contact("Jan", "Kowalski", new Date("2023-09-15"));
 const Person = new Contact("Jan", "Kowalski" );
-
-console.log(Person);
-
-console.log(Person.getLastName());
-console.log(Person.getName());
+// console.log(Person);
+// console.log(Person.getLastName());
+// console.log(Person.getName());
 Person.setName("Mietek");
-console.log(Person.getName());
+// console.log(Person.getName());
+Person.setEmail('Jan.Kowalski@gmail.com');
+// console.log(Person);
+Person.setExternalUUID('9876-5432-1098');
+// console.log(Person.getUUID());
+
+const Grupa = new Group('Nowa grupa', 'Nowy UUID');
+const Grupa1 = new Group('Grupa1','UUID_1111');
+// console.log(Grupa);
+Grupa.addContactToGroup(Person);
+// console.log(Grupa);
+const Person1 = new Contact("Janina", "Kowalska" );
+Grupa.addContactToGroup(Person1);
+console.log(Grupa);
+Grupa1.addContactToGroup(Person);
+
+const AdresBuk = new AddressBook();
+AdresBuk.addContact(Person);
+AdresBuk.addGroup(Grupa);
+AdresBuk.addGroup(Grupa1);
+// console.log(AdresBuk);
+// console.log(AdresBuk.allGroups.forEach(e => e.contactList.forEach(c => console.log(c.getName()))));
+// console.log(Grupa.findContactInGroupByUIID('1234-5678-9012'));
+
+console.log("Remove:");
+Grupa.addContactToGroup(Person);
+Grupa.removeContactFromGroup(Person1);
+console.log(Grupa);
+
